ec7b316d86baf0a7e504ae59da66da33c4292d95
paulapanetta <paulapanetta7@gmail.com>
2025-10-03 18:46:13 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+$items = [
+    [
+        'nombre' => 'Crepusculo',
+        'categoria' => 'Romance',
+        'descripcion' => 'Bella Swan se va a vivir con su padre al pequeño pueblo de Forks, donde conoce a Edward, un atractivo y misterioso chico del que se enamora y quien esconde un gran secreto: es un vampiro.',
+        'imagen' => 'https://es.web.img2.acsta.net/pictures/14/07/09/10/00/277089.jpg'
+    ],
+    [
+        'nombre' => 'Yo antes de ti',
+        'categoria' => 'Romance',
+        'descripcion' => 'La vida de Louisa Clark, una chica alegre y alocada, que va empalmando un trabajo con otro para ayudar a su familia a subsistir, cambia por completo cuando comienza a trabajar como cuidadora de un joven millonario, quien quedó paralítico tras un accidente. ',
+        'imagen' => 'https://es.web.img3.acsta.net/pictures/16/02/04/15/49/599815.jpg'
+    ],
+    [
+        'nombre' => 'Ratatouille',
+        'categoria' => 'Infantil',
+        'descripcion' => 'La rata Remy viaja a París para cumplir su sueño: convertirse en un gran chef. Aunque las ratas no pueden entrar en las cocinas, se hace amigo de un chico mediocre que trabaja en un restaurante de lujo.',
+        'imagen' => 'https://es.web.img3.acsta.net/medias/nmedia/18/67/30/37/18976643.jpg'
+    ],
+    [
+        'nombre' => 'Buscando a nemo',
+        'categoria' => 'Infantil',
+        'descripcion' => 'Marlin, un pez payaso, siempre ha intentado proteger de todos los peligros a su hijo. Sin embargo, un buzo atrapa al pequeño, y ahora el padre deberá embarcarse en una increíble aventura por las aguas australianas para encontrarlo.',
+        'imagen' => 'https://es.web.img3.acsta.net/pictures/14/02/13/11/08/054573.jpg'
+    ],
+    [
+        'nombre' => 'Los increibles',
+        'categoria' => 'Infantil',
+        'descripcion' => 'Un superhéroe retirado lucha contra el aburrimiento y, junto a su familia, tiene la oportunidad de salvar al mundo.',
+        'imagen' => 'https://pics.filmaffinity.com/Los_Increaibles-939387273-large.jpg'
+    ],
+    [
+        'nombre' => 'Tiempos violentos',
+        'categoria' => 'Crimen',
+        'descripcion' => 'La vida de un boxeador, dos sicarios, la esposa de un gánster y dos bandidos se entrelaza en una historia de violencia y redención.',
+        'imagen' => 'https://es.web.img3.acsta.net/c_310_420/img/05/66/05663f00b8b5df58b003aaf5c46ef8ad.jpg'
+    ],
+    [
+        'nombre' => 'Parasitos',
+        'categoria' => 'Crimen',
+        'descripcion' => 'Tanto Gi Taek como su familia están sin trabajo. Cuando su hijo mayor, Gi Woo, empieza a impartir clases particulares en la adinerada casa de los Park, las dos familias, que tienen mucho en común pese a pertenecer a dos mundos totalmente distintos, entablan una relación de resultados imprevisibles.',
+        'imagen' => 'https://m.media-amazon.com/images/M/MV5BYjk1Y2U4MjQtY2ZiNS00OWQyLWI3MmYtZWUwNmRjYWRiNWNhXkEyXkFqcGc@._V1_.jpg'
+    ],
+    [
+        'nombre' => 'El exorcista',
+        'categoria' => 'Terror',
+        'descripcion' => 'Una actriz llama a unos sacerdotes jesuitas para que intenten terminar con la posesión demoníaca de su hija de 12 años.',
+        'imagen' => 'https://es.web.img3.acsta.net/medias/nmedia/18/72/15/60/20054563.jpg'
+    ],
+    [
+        'nombre' => 'Fragmentado',
+        'categoria' => 'Terror',
+        'descripcion' => 'Kevin, un hombre psicótico con 23 personalidades, secuestra a 3 chicas jóvenes y las mantiene retenidas en un sótano. A medida que una de sus personalidades va imponiéndose al resto, la vida de las chicas, y la del propio Kevin, peligra cada vez más.',
+        'imagen' => 'https://mx.web.img2.acsta.net/pictures/17/10/17/08/11/3054670.jpg'
+    ],
+    [
+        'nombre' => 'El conjuro',
+        'categoria' => 'Terror',
+        'descripcion' => 'A principios de los años 70, Ed y Lorrain Warren, reputados investigadores de fenómenos paranormales, se enfrentan a una entidad demoníaca al intentar ayudar a una familia que está siendo aterrorizada por una presencia oscura en su aislada granja.',
+        'imagen' => 'https://mx.web.img3.acsta.net/pictures/19/03/20/19/26/1988298.jpg'
+    ],
+];
